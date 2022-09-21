05a457b457c60501fc7d89608a3ef1b6d00dc73e
Daniel Ortiz <thisisdanielortiz@gmail.com>
2022-09-21 14:32:28 -0500

    handling exceptios in db

--- config/db.py
+++ config/db.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import declarative_base, Session
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine("mysql+pymysql://root:Dex122706@localhost:3306/users")
+USER = "root"
+PASSWORD = "Dex122706"
+DB = "users"
+
+engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@localhost:3306/{DB}")
 sessionmaker = sessionmaker(bind=engine,autocommit=False, autoflush=False)
 session = scoped_session(sessionmaker)
 Base = declarative_base()
