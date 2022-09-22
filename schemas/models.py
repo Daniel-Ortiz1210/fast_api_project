1877cf970cb8dd355ba8cae278a6a7dbc427c04d
Daniel Ortiz <thisisdanielortiz@gmail.com>
2022-09-22 13:32:10 -0500

    Modify User Model

--- schemas/models.py
+++ schemas/models.py
@@ -1,8 +1,12 @@
 # Pydantic
-from pydantic import BaseModel, Field, PositiveInt, EmailStr
+from pydantic import BaseModel, Field, PositiveInt, EmailStr, validator, ValidationError
 from typing import Optional, List
 from enum import Enum
 from datetime import date,datetime
+
+
+def normalize_user_names(name: str) -> str:
+    return name.title()
 
 class UserType(str, Enum):
     admin = "admin"
@@ -14,16 +18,20 @@ class User(BaseModel):
     id: Optional[int] = Field()
     first_name: str = Field(..., min_length=1, max_length=50)
     last_name: str = Field(..., min_length=1, max_length=50)
-    email: EmailStr = Field(..., title="User email")
-    password: str = Field(..., min_length=8, max_length=50)
+    email: EmailStr = Field(..., title="Email")
+    password: str = Field(..., min_length=8, max_length=50, title="Constraseña")
     
-    curp: Optional[str] = Field(None, max_length=18)
-    rfc: Optional[str] = Field(None, max_length=13)
-    cp: Optional[str] = Field(None, max_length=5)
+    curp: Optional[str] = Field(None, title="Clave Unica de Registro de Poblacion")
+    rfc: Optional[str] = Field(None, title="Registro Federal del Contribuyente")
+    cp: Optional[str] = Field(None, title="Codigo Postal")
     telephone: Optional[str] = Field(None, max_length=10)
-    user_type: Optional[UserType] = Field(UserType.basic, title="User Type")
-    date: Optional[datetime] = Field(default=datetime.now())
-    age: Optional[PositiveInt] = Field(default=None,title="User age")
+    user_type: Optional[UserType] = Field(UserType.basic, title="Tipo de usuario")
+    date: Optional[datetime] = Field(datetime.now(), title="Fecha")
+    age: Optional[PositiveInt] = Field(None,title="Edad")
+
+    _normalize_first_name = validator("first_name", allow_reuse=True, always=True)(normalize_user_names)
+    _normalize_last_name = validator("last_name", allow_reuse=True, always=True)(normalize_user_names)
+    
 
     class Config:
         schema_extra = {
@@ -42,6 +50,22 @@ class User(BaseModel):
             }
         }
         orm_mode = True
+    
+    @validator('cp')
+    def validate_first_2_chars(cls, v):
+        if v is None: return None
+        
+        if v[:2] == '00':
+            raise ValueError("Los primeros dos caracteres no deben ser '00'")
+        if len(v) < 5 or len(v) > 5:
+            raise ValueError("La longitud requerida son 5 caracteres")
+        
+        return v
+    
+
+
+        
+
 
 
 class LoginResponse(BaseModel):
